dba81deed4ff367058ddd3bac008eb54adddbb57
Lukas Tietenberg <lukas.tietenberg@outlook.de>
2025-04-18 23:50:11 +0200

    Start PlayersController

---
+++ code/domains/players/adapter/rest/src/main/java/com/github/sourcefranke/tictactoe/players/adapter/rest/PlayersController.java
@@ -0,0 +1,8 @@
+package com.github.sourcefranke.tictactoe.players.adapter.rest;
+
+import com.github.sourcefranke.tictactoe.players.adapter.rest.api;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PlayersController implements PlayersApi {
+}
